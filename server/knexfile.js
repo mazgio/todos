81e5c7e6308797684c4d044cfd83279c8879fe77
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-12-21 20:49:21 +0100

    added sqlite3 db CRUD

---
+++ server/knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './api/todos.db3'
+    },
+    migrations: {
+      directory: './api/migrations'
+    },
+    seed: {
+      directory: './api/seeds'
+    },
+    useNullAsDefault: true,
+  }
+};
